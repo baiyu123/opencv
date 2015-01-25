af968eedeb09b9959626a9d793cacb305fdc704c
baiyu123 <baiyuhua@usc.edu>
2015-01-24 21:09:42 -0800

    template matching vid

---
+++ vidTemplate.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+template = cv2.imread('original.jpg',0)
+
+while(1)
+    ret, img_rgb = cp.read()
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    w, h = template.shape[::-1]
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    cv2.imshow('view',img_rgb)
+    if cv2.waitKey(1) & 0xFF == ord('q')
+        break
+cap.release()
+cv2.destroyAllWindows()
