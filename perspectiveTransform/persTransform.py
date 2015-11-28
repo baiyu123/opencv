0aa3104c023242c6c5137abdfd9b6f567c89e14d
Baiyu Huang <baiyuhua@usc.edu>
2015-11-27 20:03:52 -0800

    perspective finished

--- perspectiveTransform/persTransform.py
+++ perspectiveTransform/persTransform.py
@@ -7,6 +7,7 @@ pts1 = np.float32([[148,166],[354,162],[96,282],[499,256]])
 pts2 = np.float32([[300,300],[400,300],[300,400],[400,400]])
 
 M = cv2.getPerspectiveTransform(pts1,pts2)
+print M
 dst = cv2.warpPerspective(img,M,(600,600))
 
 cv2.imshow("original",img)
